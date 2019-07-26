c61343fbb8e5ab3ce20c1761ea157b85d0d22839
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-07-26 20:33:47 +0200

    ADD => POC - jobLocation

---
+++ POC - jobLocation.py
@@ -0,0 +1,34 @@
+# import libraries
+from bs4 import BeautifulSoup
+import urllib.request
+
+# Specify the Scrape URL
+pageURL =  'https://www.indeed.co.za/jobs?q=wordpress&l=Cape+Town,+Western+Cape&start=10'
+
+# Assign User-Agent to Scrape Request
+req = urllib.request.Request(
+    pageURL,
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    }
+)
+
+# Query webpage HTML Code and Assign to variable 'pageQuery'
+pageQuery = urllib.request.urlopen(req)
+
+# Parse the HTML Code using BeautifulSoup and assign parsed data to variable 'parsedHTML'
+parsedHTML = BeautifulSoup(pageQuery, 'html.parser')
+
+# Create list
+locations = []
+
+# Function - iterate through div and span elements for value and output to list
+def jobLocation(parsedHTML, locations):
+    for div in parsedHTML.find_all(name='div', attrs={'class':'sjcl'}):
+        for span in div.find_all(name='span', attrs={'class':'location'}):
+            locations.append(span.getText().strip())
+    print(locations)
+
+# Execute function => jobLocation
+jobLocation(parsedHTML, locations)
